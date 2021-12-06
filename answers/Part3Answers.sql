fa5f16cfac718179886a45a00b4497f6b6111606
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-12-06 15:51:11 -0500

    Part3-poison

--- answers/Part3Answers.sql
+++ answers/Part3Answers.sql
@@ -7,3 +7,7 @@ SELECT * FROM pokemons p LEFT JOIN types t ON p.secondary_type=t.id WHERE p.name
 # What are the names of the pokemon that belong to the trainer with trainerID 303?
 SELECT trainerID, name FROM pokemon_trainer p_t LEFT JOIN pokemons p ON p_t.pokemon_id=p.id WHERE p_t.trainerID=303;
 
+# How many pokemon have a secondary type Poison?
+SELECT COUNT(id) FROM pokemons WHERE secondary_type=7;
+
+
