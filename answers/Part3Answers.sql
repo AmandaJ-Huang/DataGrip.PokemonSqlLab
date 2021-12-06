f38bf5a4bc7c3922d667baeecbd71ee8ff243ce1
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-12-06 15:34:39 -0500

    Part3-rufflet

--- answers/Part3Answers.sql
+++ answers/Part3Answers.sql
@@ -1,2 +1,5 @@
 # What is each pokemon's primary type?
 SELECT * FROM pokemons p LEFT JOIN types t ON p.primary_type=t.id;
+
+# What is Rufflet's secondary type?
+SELECT * FROM pokemons p LEFT JOIN types t ON p.secondary_type=t.id WHERE p.name='Rufflet';
