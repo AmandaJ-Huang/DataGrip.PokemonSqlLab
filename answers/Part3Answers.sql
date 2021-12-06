81c6b90300c0ba80de4b3f86b5484778047da5d6
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-12-06 15:43:40 -0500

    Part3-trainerID 303

--- answers/Part3Answers.sql
+++ answers/Part3Answers.sql
@@ -3,3 +3,7 @@ SELECT * FROM pokemons p LEFT JOIN types t ON p.primary_type=t.id;
 
 # What is Rufflet's secondary type?
 SELECT * FROM pokemons p LEFT JOIN types t ON p.secondary_type=t.id WHERE p.name='Rufflet';
+
+# What are the names of the pokemon that belong to the trainer with trainerID 303?
+SELECT trainerID, name FROM pokemon_trainer p_t LEFT JOIN pokemons p ON p_t.pokemon_id=p.id WHERE p_t.trainerID=303;
+
